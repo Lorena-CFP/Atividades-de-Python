5263c706ff409f9bd0cf37cc6528111c69846412
Lorena Cordeiro Ferreira Pereira <lorenacordeiro.fp@gmail.com>
2025-12-10 12:51:36 -0300

    Atividade dicionario adicionada

---
+++ dicionario/dicionario.py
@@ -0,0 +1,4 @@
+cadastro = [{"nome": 'Lorena', "idade": 20, "cidade":'São josé do Rio Preto'}]
+
+for pessoa in cadastro:
+    print(f'Nome: {pessoa["nome"]}, Idade: {pessoa["idade"]}, Cidade: {pessoa["cidade"]}')
